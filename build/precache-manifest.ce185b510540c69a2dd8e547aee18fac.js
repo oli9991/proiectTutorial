202793f5b71bbee16afda24f253d388e221c69c0
Ursu Olimpia Denisa <33007961+oli9991@users.noreply.github.com>
2019-10-10 19:31:51 +0300

    Add files via upload

---
+++ build/precache-manifest.ce185b510540c69a2dd8e547aee18fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591b9f2ffe2d52e03e49f53b9c7da65d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f7cdc1789f0a7b6e04",
+    "url": "/static/css/main.c7cddcdd.chunk.css"
+  },
+  {
+    "revision": "c9651355656939d809ce",
+    "url": "/static/js/2.867b0ce7.chunk.js"
+  },
+  {
+    "revision": "56f7cdc1789f0a7b6e04",
+    "url": "/static/js/main.c42f51a3.chunk.js"
+  },
+  {
+    "revision": "3a8434b0f319e62c3556",
+    "url": "/static/js/runtime~main.2aa07886.js"
+  },
+  {
+    "revision": "87b95edf797c62e987efb132ed1ea18f",
+    "url": "/static/media/juice.87b95edf.png"
+  }
+]);
